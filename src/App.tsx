c51947441db74a5bbb1ecde92c1f7d3bc7ba5d27
bhad778 <bhad778@gmail.com>
2023-02-02 11:16:07 -0500

    added a components folder

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,15 @@ import { Provider } from "react-redux";
 import { store } from "reduxStore/store";
 import { ThemeProvider } from "theme/theme.context";
 import { DEFAULT_THEME } from "theme/default.theme";
+import SearchContainer from "components/searchContainer";
 
-import logo from "./logo.svg";
 import "./App.css";
 
 const App = () => {
   return (
     <Provider store={store}>
       <ThemeProvider initial={DEFAULT_THEME}>
-        <div style={{ backgroundColor: "red" }}>
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.tsx</code> and save to reload.
-            </p>
-            <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer"></a>
-          </header>
-        </div>
+        <SearchContainer />
       </ThemeProvider>
     </Provider>
   );
